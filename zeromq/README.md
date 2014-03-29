b70e752be42af272a1de603be863a612b8af29e5
Matt Warren <matt.warren@gmail.com>
2014-03-29 11:36:26 -0600

    adding simple zmq example

---
+++ zeromq/README.md
@@ -0,0 +1,14 @@
+file information over zmq
+====
+
+code from Node.js the Right Way Chapter 4
+
+client/server that uses zmq to communicate.
+
+run the server:
+
+    node --harmony zmq-filer-rep.js
+
+run the client:
+
+    node --harmony zmq-filer-req.js target.txt
---
+++ zeromq/zmq-filer-rep.js
@@ -0,0 +1,29 @@
+'use strict';
+const
+  fs = require('fs'),
+  zmq = require('zmq'),
+  responder = zmq.socket('rep');
+
+responder.on('message', function(data) {
+  let request = JSON.parse(data);
+  console.log('recieveed request to get ' + request.path);
+
+  //read file and reply
+  fs.readFile(request.path, function(err, content){
+    console.log('Sending response content');
+    responder.send(JSON.stringify({
+      content: content.toString(),
+      timestamp: Date.now(),
+      pid: process.pid
+    }));
+  });
+});
+
+responder.bind('tcp://127.0.0.1:5433', function(err) {
+  console.log('Listening for zmq requesters...');
+});
+
+process.on('SIGINT', function() {
+  console.log('Shutting down...');
+  responder.close();
+});
